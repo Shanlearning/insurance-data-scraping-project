576fc3059d2e6832f83f6aecb306fed2f69cf365
Shan Zhong <zhongshanyes@gmail.com>
2019-07-07 22:44:38 +0800

    Shan

--- spiders/chinalife.py
+++ spiders/chinalife.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from project_insurance_scrap.items import  ProjectInsuranceScrapItem
-
+import re
 
 class ChinalifeSpider(scrapy.Spider):
     # 抓取机名字
     name = '中国人寿'
     cn_name = '中国人寿'
     # 允许的抓取范围
-    allowed_domains = ['http://www.e-chinalife.com/']
-    
+    next_url = 'http://www.e-chinalife.com/help-center/xiazaizhuanqu/tingbanbaoxianchanpin.html&curtPage='
     def start_requests(self):
         # 输入在售保险的第一页网址
         zaishou_urls = [
@@ -23,7 +22,7 @@ class ChinalifeSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.tingshou_parse)
             
 
-    def zaishou_parse(self, response,cn_name):                
+    def zaishou_parse(self, response,cn_name=name,next_url=next_url):                
         # 从每一行抽取数据
         for part in response.css('.downlist li'):
             # 在售保险的内容输入
@@ -37,12 +36,12 @@ class ChinalifeSpider(scrapy.Spider):
             yield item 
         
         # 找到下一页的代码
-        next_page = response.css('li.next a::attr(href)').get()
+        next_page = response.css('.page_down::attr(onclick)').get()
         if next_page is not None:
-            next_page = response.urljoin(next_page)
+            next_page = next_url +str(re.findall("\d+",next_page))
             yield scrapy.Request(next_page, callback=self.zaishou_parse)
             
-    def tingshou_parse(self, response，cn_name):                
+    def tingshou_parse(self, response,cn_name=name,next_url=next_url):                
         # 从每一行抽取数据
         for part in response.css('.downlist li'):
             # 停售保险的内容输入
@@ -56,9 +55,9 @@ class ChinalifeSpider(scrapy.Spider):
             yield item 
         
         # 找到下一页的代码
-        next_page = response.css('li.next a::attr(href)').get()
+        next_page = response.css('.page_down::attr(onclick)').get()
         if next_page is not None:
-            next_page = response.urljoin(next_page)
+            next_page = next_url +str(re.findall("\d+",next_page))
             yield scrapy.Request(next_page, callback=self.tingshou_parse)
     
 
