b0fe9e480ce4d00cb7fa48ecc81155ddc22ff71c
Shan Zhong <zhongshanyes@gmail.com>
2019-07-14 16:03:47 +0800

    Shan

--- project_insurance_scrap/scrapfunctions.py
+++ project_insurance_scrap/scrapfunctions.py
@@ -6,3 +6,11 @@ def shan_keep(filter,dat):
         if str(filter) in part:
             keep.append(part)
     return keep
+
+
+def str_extract(extract,dat):
+    keep = []
+    for part in dat:
+        if str(filter) in part:
+            keep.append(part)
+    return keep
